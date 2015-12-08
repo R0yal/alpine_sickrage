d1f779005fcb2e797de2eabd7c9f712e52a3cad5
R0yal <R0yal@openmailbox.org>
2015-12-08 18:02:54 +1100

    Resolves missing fi

--- alpine_sickrage.sh
+++ alpine_sickrage.sh
@@ -10,6 +10,7 @@ if [[ "$(grep "$user" /etc/passwd|wc -l)" -eq 0]]; then
   adduser -S $user
 else
   echo 'SickRage user already exists: ' $user
+fi
 echo 'Creating /opt directory'
 if [ ! -d "$dir" ]; then
   mkdir /opt
